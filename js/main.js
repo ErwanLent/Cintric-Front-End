fcf11fca258bbea391ae344c91fadd5bbdebf715
Erwan Lent <erwanlent@gmail.com>
2014-09-15 05:28:36 -0400

    Smoothed out chat animations.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $('document').ready(function(){
 
+	// Fixes IE + Firefox scrolling bug
+	// setTimeout(function(){
+	// 	window.scrollTo(0, 0);
+	// }, 200);
+
+    /*=====================================================================================
+        Header Animation
+    =======================================================================================*/
+	
 	setTimeout(function(){
 		$('.logo').removeClass('animated');
 		$('.logo').removeClass('fadeInDownBig');
@@ -10,6 +19,25 @@ $('document').ready(function(){
 		$('.logo-text').removeClass('hidden');
 		//$('.marvel-device.nexus5').addClass('landscape');
 	}, 1050);
+
+    /*=====================================================================================
+        Messenger Animation
+    =======================================================================================*/
+
+	var messages = ["Yes, what do you need?", "Where is the French cheese?", "The French cheese can be found in France.", "Wow, you're so helpful. Thanks!"];
+	var messageCounter = 0;
+
+	setInterval(function(){
+
+		if (messageCounter < messages.length)
+		{
+			var messageEntry = generateListEntry(messageCounter, messages[messageCounter]);
+			messageCounter++;
+
+			insertListEntry(messageEntry, true);
+		}
+
+	}, 2000);
 
     /*=====================================================================================
         Sticky Navigation
@@ -34,9 +62,47 @@ $('document').ready(function(){
 			$('.section.first').css('margin-top', '0px');
 			isNavigationStuck = false;
 		}
-
-
 	});
 
 
 });
+
+
+function generateListEntry(messageCounter, message)
+{
+	var listEntry = $('#chat-entry').html();
+	var image;
+
+	switch((messageCounter + 1) % 2)
+	{
+		case 0:
+			name = "Camelia Moher";
+			image = "camelia.jpg";
+			break;
+		case 1:
+			name = "Kroger Representative";
+			image = "kroger.svg";
+			break;
+	}
+
+	listEntry = listEntry.replace("%NAME%", name);
+	listEntry = listEntry.replace("%MESSAGE%", message);
+	listEntry = listEntry.replace("%IMAGE%", image);
+
+	return listEntry;
+}
+
+function insertListEntry(entry, isAnimated)
+{
+	$('.chat-history').append(entry);
+
+	var lastEntryTopPosition = $($('.chat-history').children()[$('.chat-history').children().length - 1]).position().top;
+
+	if (isAnimated)
+	{
+		$('.chat-history').stop();
+		$('.chat-history').animate({
+			scrollTop: lastEntryTopPosition + $('.chat-history').scrollTop()
+	    }, 2000);
+	}
+}
